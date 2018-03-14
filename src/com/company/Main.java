96adcc054dc20b2a306d9ead1edec5cddaddc528
Piatnytskyi Andrii <pyatnitskiy.a@gmail.com>
2018-03-14 23:29:25 +0200

    setPriority(), join(), yield(), sleep()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,17 +2,33 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread thread = new MyThread();
-        Thread runnable = new Thread(new MyRunnable());
+        Thread thread1 = new MyThread();
+        //Устанавливаем приоритет потока
+        thread1.setPriority(Thread.MAX_PRIORITY);
 
         thread.start();
+        //Поток main и остальные потоки ждут, пока поток thread не закончит полностью свою работу.
+        thread.join();
 
-        for (int i = 0; i < 1000; i++) {
+        //Сообщаем что данный поток может на время уйти в пул потоков
+        Thread.yield();
+        thread1.start();
+
+        testPrint();
+    }
+
+    public static void testPrint() {
+        for (int i = 0; i < 10; i++) {
+            //Поток засыпает
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println(Thread.currentThread().getName() + "     >>> " + i);
         }
-
-        runnable.start();
     }
 
 }
@@ -20,17 +36,8 @@ public class Main {
 class MyThread extends Thread {
     @Override
     public void run() {
-        for (int i = 0; i < 1000; i++) {
-            System.out.println(Thread.currentThread().getName() + "     >>> " + i);
-        }
+        Main.testPrint();
     }
 }
 
-class MyRunnable implements Runnable{
-    @Override
-    public void run() {
-        for (int i = 0; i < 1000; i++) {
-            System.out.println(Thread.currentThread().getName() + "     >>> " + i);
-        }
-    }
-}
+
