eed68067a530ffa5fcc74dcb958b2ae3b829d87a
Piatnytskyi Andrii <pyatnitskiy.a@gmail.com>
2018-03-15 01:24:14 +0200

    synchronized

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,40 +3,78 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        Thread thread = new MyThread();
-        Thread thread1 = new MyThread();
-        //Устанавливаем приоритет потока
-        thread1.setPriority(Thread.MAX_PRIORITY);
-
-        thread.start();
-        //Поток main и остальные потоки ждут, пока поток thread не закончит полностью свою работу.
-        thread.join();
-
-        //Сообщаем что данный поток может на время уйти в пул потоков
-        Thread.yield();
+        Resources resources = new Resources();
+        MyThread1 thread1 = new MyThread1();
+        MyThread2 thread2 = new MyThread2();
+        thread1.setResources(resources);
+        thread2.setResources(resources);
         thread1.start();
+        thread2.start();
 
-        testPrint();
+//        Thread thread1 = new MyThread();
+//        //Устанавливаем приоритет потока
+//        thread1.setPriority(Thread.MAX_PRIORITY);
+//
+//        thread.start();
+//        //Поток main и остальные потоки ждут, пока поток thread не закончит полностью свою работу.
+//        thread.join();
+//
+//        //Сообщаем что данный поток может на время уйти в пул потоков
+//        Thread.yield();
+//        thread1.start();
+
     }
 
-    public static void testPrint() {
-        for (int i = 0; i < 10; i++) {
-            //Поток засыпает
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(Thread.currentThread().getName() + "     >>> " + i);
-        }
-    }
+
 
 }
 
-class MyThread extends Thread {
+class MyThread1 extends Thread {
+    Resources resources;
+
+    public void setResources(Resources resources) {
+        this.resources = resources;
+    }
+
     @Override
     public void run() {
-        Main.testPrint();
+        for (int i = 0; i < 500; i++) {
+            resources.increaseI();
+            System.out.println(">>>     " + Thread.currentThread().getName());
+            System.out.println(">>>     " + resources.i);
+        }
+    }
+}
+class MyThread2 extends Thread {
+    Resources resources;
+
+    public void setResources(Resources resources) {
+        this.resources = resources;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5000; i++) {
+            resources.increaseI();
+            System.out.println(Thread.currentThread().getName());
+            System.out.println(resources.i);
+        }
+    }
+}
+
+class Resources{
+    int i;
+
+    //Блокирует вызов метода другим потоком, пока данный поток не отрабоатет полностью метод
+    //Также можно блокировать чать кодав synchronized блоке
+    public synchronized void increaseI(){
+        int i = this.i;
+
+        if (Thread.currentThread().getName().equals("Thread-1")){
+            Thread.yield();
+        }
+        i++;
+        this.i = i;
     }
 }
 
