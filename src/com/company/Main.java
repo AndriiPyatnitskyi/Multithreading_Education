bb5e3452cf74357921ff8e9c922f5899c31be902
Piatnytskyi Andrii <pyatnitskiy.a@gmail.com>
2018-03-15 15:48:24 +0200

    deadlock

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,60 @@
 package com.company;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import sun.awt.windows.ThemeReader;
 
 public class Main {
-    static AtomicInteger atomicInteger = new AtomicInteger(0);
 
-    public static void main(String[] args) throws InterruptedException {
-        for (int j = 0; j < 1000; j++) {
-            new MyThread().start();
-        }
-        //засыпаем, чтоб потоки со счетчиком успели до конца отрабоать
-        Thread.sleep(1000);
-        System.out.println(atomicInteger.get());
+    public static void main(String[] args) {
+        ResourceA resourceA = new ResourceA();
+        ResourceB resourceB = new ResourceB();
+
+        resourceA.setResourceB(resourceB);
+        resourceB.setResourceA(resourceA);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Стартуем " + Thread.currentThread().getName() + " поток");
+                resourceB.startBlock();
+
+            }
+        }).start();
+        System.out.println("Стартуем " + Thread.currentThread().getName() + " поток");
+        resourceA.startBlock();
     }
 
-    static class MyThread extends Thread {
-        @Override
-        public void run() {
-            atomicInteger.incrementAndGet();
+    static class ResourceA {
+        ResourceB resourceB;
+
+        public void setResourceB(ResourceB resourceB) {
+            this.resourceB = resourceB;
         }
+
+        public synchronized void startBlock() {
+            System.out.println(this.getClass().getName() + "    >>> startBlock");
+            resourceB.methodB();
+        }
+
+        public synchronized void methodA() {
+            System.out.println("methodA : вывод после блокировки");
+        }
+    }
+
+    static class ResourceB {
+        ResourceA resourceA;
+
+        public void setResourceA(ResourceA resourceA) {
+            this.resourceA = resourceA;
+        }
+
+        public synchronized void startBlock() {
+            System.out.println(this.getClass().getName() + "    >>> startBlock");
+            resourceA.methodA();
+        }
+
+        public synchronized void methodB() {
+            System.out.println("methodB: вывод после блокировки");
+        }
+
     }
 }
 
